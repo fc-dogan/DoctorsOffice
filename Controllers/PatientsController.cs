85a3ad47f1fcf542e0ad05092ce8cb07d7c3a524
Jieun Kang <jieunkang101@gmail.com>
2020-03-23 16:50:49 -0700

    update PatientsController.cs and add Patient/Index.cshtml
    > Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    > Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    > Co-authored-by: Jieun Kang <jieunkang101@gmail.com>

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      // List<Item> model = _db.Items.Include(items => items.Category).ToList();
+      // return View(model);
+      return View(_db.Items.ToList());
+    }
+  }
+}  
