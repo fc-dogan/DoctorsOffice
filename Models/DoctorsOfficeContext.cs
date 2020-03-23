40453d5ad2d1da723ccdbae5abd4b65125a24c03
Jieun Kang <jieunkang101@gmail.com>
2020-03-23 13:37:27 -0700

    create DesignTime and DoctorsOfficeContext files
    > Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    > Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    > Co-authored-by: Jieun Kang <jieunkang101@gmail.com>

---
+++ Models/DoctorsOfficeContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorsOffice.Models
+{
+  public class DoctorsOfficeContext : DbContext
+  {
+    public virtual DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<DoctorPatient> DoctorPatient { get; set; }
+    public DoctorsOfficeContext(DbContextOptions options) : base(options) { }
+  }
+}
